cd905f897c62c1800144bebab79862a6e57c9b21
Ashish Rai <99983844+MythicPlayer1@users.noreply.github.com>
2023-02-07 14:03:50 +0545

    Add files via upload

--- view/src/components/Container.js
+++ view/src/components/Container.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import { Button } from '@mui/material'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import EditNotificationsOutlinedIcon from '@mui/icons-material/EditNotificationsOutlined';
+import React, { useState } from 'react'
+import './Container.css'
 
 export const Container = () => {
+  const [NewText, setNewText] = useState();
+  const [submit, setSubmit] = useState()
+
+  const textchangeHandler = (event) => {
+    setNewText(event.target.value);
+  }
+
+  const submithandler = (event) => {
+   
+      event.preventDefault();
+      const data={
+        submit: NewText
+      }
+      console.log(data)
+      setNewText(" ");
+      setSubmit(" ")
+  }
   return (
-    <div>
-        <label></label>
-        <input type='text' value=''></input>
+    <div className='container'>
+      <label></label>
+      <input className='input-txt' type='text' onChange={textchangeHandler} value={NewText} placeholder='Add a text'></input>
+      <div className='sub-container-1'>
+        <div className='inner-container-1'>
+          <CalendarMonthIcon style={{ color: 'rgb(90, 90, 90)' }}></CalendarMonthIcon>
+          <EditNotificationsOutlinedIcon style={{ color: 'rgb(90, 90, 90)' }}></EditNotificationsOutlinedIcon>
+        </div>
+        <button className='Button' onClick={submithandler}>Add</button>
+      </div>
+
     </div>
   )
 }

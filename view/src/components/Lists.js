65d901c4657e4505c1183c08035217e6a31c7e3e
Ashish Rai <99983844+MythicPlayer1@users.noreply.github.com>
2023-02-20 21:33:14 +0545

    Add files via upload

--- view/src/components/Lists.js
+++ view/src/components/Lists.js
@@ -1,14 +1,40 @@
 
 import React from 'react'
- import './Container.css'
+import './Container.css'
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import { IconButton } from '@mui/material';
 
 export const Lists = (props) => {
-  
+
+  const deletehandler=()=>{
+
+  }
+
+  const edithandler=()=>{
+
+  }
   return (
     <div className='top-main'>
-    <div className='main-list'>
-        <div className='list-input' type='text' >{props.item.NewText}</div>
+      <div className='main-list'>
+        <div className='list-input' >
+          <div> {props.item.NewText}</div>
+          <div>
+          <IconButton 
+            onClick={deletehandler}
+            >
+            <DeleteOutlineIcon></DeleteOutlineIcon>
+          </IconButton>
+          <IconButton onClick={edithandler}>
+              <ModeEditOutlineIcon></ModeEditOutlineIcon>
+          </IconButton>
 
-    </div></div>
+          </div>
+         
+        </div>
+
+
+
+      </div></div>
   )
 }
